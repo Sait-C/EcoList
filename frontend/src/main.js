2970fa43fa677992ac8b5c1c6749ce8a2a8a684b
ggamzed <gamzeddmrc@gmail.com>
2025-05-05 21:25:12 +0300

    writing changes in main page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,6 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 const toastOptions = { timeout: 3000 };
 
-// Custom Components & Directives
 import globalComponent from "./plugins/global-components";
 
 const app = createApp(App);
@@ -17,10 +16,8 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 
-//Library Components
 app.use(Toast, toastOptions);
 
-// Custom Components & Directives
 app.use(globalComponent);
 
 app.mount("#app");
